7e4e0070912d48d5d944e33068bc16d03945eed6
Tiffceet <zkang.loo@gmail.com>
2022-03-02 20:04:59 +0800

    Update DBSetup.bat
    Add: Status message to ease future debugging (if needed at all lol)

--- DBSetup.bat
+++ DBSetup.bat
@@ -1,9 +1,16 @@
 @echo off
 rem Running this batch file will reset DB 
+
+echo Stopping ArtDBInstance...
 SqlLocalDb stop "ArtDBInstance"
+echo Deleting ArtDBInstance...
 SqlLocalDb delete "ArtDBInstance"
+
+echo Deleting ArtDB...
 del %userprofile%\ArtDB.mdf
 del %userprofile%\ArtDB_log.ldf
+
+echo Creating ArtDBInstance
 SqlLocalDb create "ArtDBInstance"
 sqlcmd -S "(localdb)\ArtDBInstance" -Q "CREATE DATABASE ArtDB;"
 sqlcmd -S "(localdb)\ArtDBInstance" -i "DB_SCHEMA.sql" -o "DBSetup.log"
