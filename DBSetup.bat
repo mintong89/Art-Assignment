18697ff0e711bd338fa96061cb57f1959f69d856
Tiffceet <zkang.loo@gmail.com>
2022-03-02 19:59:07 +0800

    Update: DBSetup now logs to DBSetup.log after running DB_SCHEMA

--- DBSetup.bat
+++ DBSetup.bat
@@ -1,3 +1,4 @@
+@echo off
 rem Running this batch file will reset DB 
 SqlLocalDb stop "ArtDBInstance"
 SqlLocalDb delete "ArtDBInstance"
@@ -5,4 +6,4 @@ del %userprofile%\ArtDB.mdf
 del %userprofile%\ArtDB_log.ldf
 SqlLocalDb create "ArtDBInstance"
 sqlcmd -S "(localdb)\ArtDBInstance" -Q "CREATE DATABASE ArtDB;"
-sqlcmd -S "(localdb)\ArtDBInstance" -i "DB_SCHEMA.sql"
+sqlcmd -S "(localdb)\ArtDBInstance" -i "DB_SCHEMA.sql" -o "DBSetup.log"
