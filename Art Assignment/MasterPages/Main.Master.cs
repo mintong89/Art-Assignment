d91da6a563647346f43477fe2cb21f519032f4a0
Tiffceet <zkang.loo@gmail.com>
2022-03-05 14:41:22 +0800

    Add: JWT and Newtonsoft.JSON nuget package

--- Art Assignment/MasterPages/Main.Master.cs
+++ Art Assignment/MasterPages/Main.Master.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using JWT;
+using JWT.Algorithms;
+using JWT.Serializers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,28 @@ namespace Art_Assignment
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Create authentication payload like this
+            var payload = new Dictionary<string, object>{
+                { "uid", 69 }
+            };
 
+            // How to sign
+            var token = Utility.Auth.sign(payload);
+
+            // How to parse
+            var parsedPayload = Utility.Auth.parsePayload(token);
+
+            // How to store token into Session
+            Session["JWT"] = token;
+
+            // How to retrieve token from Session
+            Console.WriteLine(Session["JWT"]);
+
+            // How to check if token exists
+            if(Session["JWT"] == null)
+            {
+
+            }
         }
     }
 }
---
+++ Art Assignment/Utility/Auth.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using JWT;
+using JWT.Serializers;
+using JWT.Algorithms;
+using JWT.Exceptions;
+using System.Configuration;
+using Newtonsoft.Json;
+namespace Art_Assignment.Utility
+{
+    public class Auth
+    {
+        private static string auth_secret = ConfigurationManager.AppSettings["auth_secret"].ToString();
+
+        public static string sign(Dictionary<string, object> payload)
+        {
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtEncoder encoder = new JwtEncoder(algorithm, serializer, urlEncoder);
+
+            string token = encoder.Encode(payload, auth_secret);
+            return token;
+        }
+
+        public static Dictionary<string, object> parsePayload(string token)
+        {
+            if(!verify(token))
+            {
+                throw new Exception("Invalid / Expired Token");
+            }
+
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IDateTimeProvider provider = new UtcDateTimeProvider();
+            IJwtValidator validator = new JwtValidator(serializer, provider);
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+            IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+            IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+            var json = decoder.Decode(token, auth_secret, verify: true);
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+        }
+
+        public static bool verify(string token)
+        {
+            try
+            {
+                IJsonSerializer serializer = new JsonNetSerializer();
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                IJwtValidator validator = new JwtValidator(serializer, provider);
+                IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+                IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
+                IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
+
+                var json = decoder.Decode(token, auth_secret, verify: true);
+                Console.WriteLine(json);
+            }
+            catch (TokenExpiredException)
+            {
+                Console.WriteLine("Token has expired");
+                return false;
+            }
+            catch (SignatureVerificationException)
+            {
+                Console.WriteLine("Token has invalid signature");
+                return false;
+            }
+            return true;
+        }
+    }
+}
