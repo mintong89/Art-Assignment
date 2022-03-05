04eeb2fb62bd887b7a9d60e432bd020f81a7050c
Tiffceet <zkang.loo@gmail.com>
2022-03-05 16:32:36 +0800

    Add: blank UserProfile.css

--- Art Assignment/MasterPages/UserProfile.master.designer.cs
+++ Art Assignment/MasterPages/UserProfile.master.designer.cs
@@ -24,6 +24,15 @@ namespace Art_Assignment.MasterPages
         protected global::System.Web.UI.WebControls.ContentPlaceHolder subtitle;
 
         /// <summary>
+        /// UserProfileSidebar control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Art_Assignment.UserControls.UserProfileSidebar UserProfileSidebar;
+
+        /// <summary>
         /// content control.
         /// </summary>
         /// <remarks>
--- Art Assignment/UserControls/UserProfileSidebar.ascx
+++ Art Assignment/UserControls/UserProfileSidebar.ascx
@@ -1,1 +1,7 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="UserProfileSidebar.ascx.cs" Inherits="Art_Assignment.UserControls.UserProfileSidebar" %>
+<div class="flex flex-col">
+    <div>
+        <i class="fas fa-lg fa-search"></i>
+        <div> Item 1</div>
+    </div>
+</div>
