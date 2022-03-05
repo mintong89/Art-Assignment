16212bf99c9fc6ef5f643ebb253d8c126e8988b1
Tiffceet <zkang.loo@gmail.com>
2022-03-05 15:24:49 +0800

    Add token expiry when signing JWT

--- Art Assignment/Utility/Auth.cs
+++ Art Assignment/Utility/Auth.cs
@@ -5,15 +5,26 @@ using JWT.Serializers;
 using JWT.Algorithms;
 using JWT.Exceptions;
 using System.Configuration;
-using Newtonsoft.Json;
 namespace Art_Assignment.Utility
 {
     public class Auth
     {
         private static string auth_secret = ConfigurationManager.AppSettings["auth_secret"].ToString();
 
+        // How long before token will expire (Seconds)
+        private static int TOKEN_EXPIRY = 60 * 60 * 24;
+
         public static string sign(Dictionary<string, object> payload)
         {
+            if(!payload.ContainsKey("exp"))
+            {
+                IDateTimeProvider provider = new UtcDateTimeProvider();
+                var now = provider.GetNow();
+
+                double secondsSinceEpoch = UnixEpoch.GetSecondsSince(now);
+                payload["exp"] = secondsSinceEpoch + TOKEN_EXPIRY;
+            }
+
             IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
             IJsonSerializer serializer = new JsonNetSerializer();
             IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
@@ -37,8 +48,8 @@ namespace Art_Assignment.Utility
             IJwtAlgorithm algorithm = new HMACSHA256Algorithm(); // symmetric
             IJwtDecoder decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
 
-            var json = decoder.Decode(token, auth_secret, verify: true);
-            return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            var json = decoder.DecodeToObject<Dictionary<string, object>>(token, auth_secret, true);
+            return json;
         }
 
         public static bool verify(string token)
