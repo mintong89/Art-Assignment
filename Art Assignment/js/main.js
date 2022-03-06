c7ab73a650414c8f99f0ef555a0c12a937c58337
Tiffceet <zkang.loo@gmail.com>
2022-03-06 16:39:42 +0800

    Close all dropdown when opening one

--- Art Assignment/js/main.js
+++ Art Assignment/js/main.js
@@ -3,6 +3,7 @@
 
     let dd = $(".dropdown-cont", e);
     if (dd.css("top") == "0px") {
+        closeAllDropdown();
         dd.css("display", "flex");
         dd.css("top", `calc(${e.css("height")} + .5rem)`);
         dd.css("opacity", 1);
@@ -15,6 +16,17 @@
         function () {
             dd.css("display", "none");
         })
+    }
+}
+
+function closeAllDropdown() {
+    let dds = $(".dropdown-cont")
+
+    for (let i = 0; i < dds.length; i++) {
+        let dd = $(dds[i]);
+        dd.css("top", 0)
+        dd.css("opacity", 0)
+        dd.css("display", "none")
     }
 }
 
