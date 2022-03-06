94590bdbb82a18c64506488c4bb02460d94828fb
Tiffceet <zkang.loo@gmail.com>
2022-03-06 13:53:49 +0800

    Add: JS and CSS needed for toggleable dropdown

---
+++ Art Assignment/js/main.js
@@ -0,0 +1,24 @@
+﻿function dropdownOnclick() {
+    e = $(this)
+
+    let dd = $(".dropdown-cont", e);
+    if (dd.css("top") == "0px") {
+        dd.css("display", "flex");
+        dd.css("top", `calc(${e.css("height")} + .5rem)`);
+        dd.css("opacity", 1);
+    } else {
+        dd.animate({
+            top: 0,
+            opacity: 0
+        },
+        100,
+        function () {
+            dd.css("display", "none");
+        })
+    }
+}
+
+$(function () {
+    // Register all dropdowns
+    $(".dropdown-menu-item").on("click", dropdownOnclick)
+});
