6131aeeba0b64f29c7c770e028cad6a259ef4de2
Tiffceet <zkang.loo@gmail.com>
2022-03-13 13:46:44 +0800

    Make DBSetup fancy

--- scripts/DBSetup.bat
+++ scripts/DBSetup.bat
@@ -1,16 +1,39 @@
 @echo off
 rem Running this batch file will reset DB 
-
-echo Stopping ArtDBInstance...
+echo.
+echo [31mStopping ArtDBInstance...[0m
+echo.
 SqlLocalDb stop "ArtDBInstance"
-echo Deleting ArtDBInstance...
+
+echo.
+echo [31mDeleting ArtDBInstance...[0m
+echo.
 SqlLocalDb delete "ArtDBInstance"
 
-echo Deleting ArtDB...
+echo.
+echo [31mDeleting ArtDB...[0m
+echo.
 del %userprofile%\ArtDB.mdf
 del %userprofile%\ArtDB_log.ldf
 
-echo Creating ArtDBInstance
+echo.
+echo [32mCreating ArtDBInstance...[0m
+echo.
 SqlLocalDb create "ArtDBInstance"
 sqlcmd -S "(localdb)\ArtDBInstance" -Q "CREATE DATABASE ArtDB;"
 sqlcmd -S "(localdb)\ArtDBInstance" -i "DB_SCHEMA.sql" -o "DBSetup.log"
+type DBSetup.log
+
+echo.
+echo [33mRunning additional script for development purpose...[0m
+echo [33mRunning PreventLocalDBAutoshutdown.bat...[0m
+echo.
+call PreventLocalDBAutoshutdown.bat
+type PreventLocalDBAutoshutdown.log
+
+echo.
+echo [33mRestarting DB...[0m
+echo.
+call StopDB.bat
+call StartDB.bat
+pause
