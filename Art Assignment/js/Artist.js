353c3f713626e57f18fff2b785959bed0e8fe8d7
Tiffceet <zkang.loo@gmail.com>
2022-03-14 01:59:46 +0800

    Fix: cancelEditProfile trigger when mouse is dragged outside

--- Art Assignment/js/Artist.js
+++ Art Assignment/js/Artist.js
@@ -1,7 +1,10 @@
 ﻿const Artist = {
     cancelEditProfile: function (event) {
-        // What gay shit is this
-        window.location.href = $("#btnArtistProfileUpdateCancel").attr('href');
+        // Stolen from https://stackoverflow.com/questions/37573608/how-to-make-modal-close-on-click-outside
+        if (!$(event.target).closest('#artist-edit-profile-modal').length && !$(event.target).is('#artist-edit-profile-modal')) {
+            // What gay shit is this
+            window.location.href = $("#btnArtistProfileUpdateCancel").attr('href');
+        }
     },
     oldImageFileSrc: "",
     inputFileOnChange: function (evt) {
@@ -17,7 +20,7 @@
 }
 
 $(function () {
-    $(".artist-edit-profile-bg-cont").on("click", Artist.cancelEditProfile)
+    $(".artist-edit-profile-bg-cont").on("mousedown", Artist.cancelEditProfile)
     $("#profilePicInput").on("change", Artist.inputFileOnChange);
     $(".artist-upload-image-cont").on("click", Artist.uploadImageOnClick)
 });
