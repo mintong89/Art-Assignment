c8db3f1c2aa07c2758a8813be6b931d848d07c58
Tiffceet <zkang.loo@gmail.com>
2022-04-11 15:32:02 +0800

    Login.aspx: Redirect user to userprofile page when user already logged in

--- Art Assignment/Pages/Login.aspx.cs
+++ Art Assignment/Pages/Login.aspx.cs
@@ -13,7 +13,13 @@ namespace Art_Assignment.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                Auth.getLogonUserUID(Request, Response);
+                Response.Redirect("/Pages/Profile/User.aspx");
+            }
+            catch (Exception ex) { 
+            }
         }
 
         protected void btnLogin_Click(object sender, EventArgs e)
